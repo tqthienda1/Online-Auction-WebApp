b7fc13c46bd647b21bb0b18604e730654406a5a6
Ngô Bảo Long <nblong23@.clc.fitus.edu.vn>
2025-11-07 20:11:18 +0700

    first server setup

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Server is live!'));
+
+app.listen(port, () => console.log(`Server is listening at http://localhost:${port}`));
