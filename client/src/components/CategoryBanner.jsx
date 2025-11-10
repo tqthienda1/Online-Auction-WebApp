ae000e321254b34afcf01d2e7b82f0f54a15d86f
Thinh <thinh@gmail.com>
2025-11-11 01:46:03 +0700

    Add: SideBar, CategoryFilter, ColorFilter, PriceFilter; Update: CategoryPage

--- client/src/components/CategoryBanner.jsx
+++ client/src/components/CategoryBanner.jsx
@@ -1,22 +1,14 @@
 import React from "react";
 
-// Dùng style inline để đặt ảnh nền
-const bannerStyle = (imageUrl) => ({
-  height: "300px", // Bạn có thể điều chỉnh
-  backgroundImage: `url(${imageUrl})`,
-  backgroundSize: "cover",
-  backgroundPosition: "center",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  color: "white",
-  textShadow: "2px 2px 4px rgba(0,0,0,0.7)",
-});
-
 const CategoryBanner = ({ title, imageUrl }) => {
   return (
-    <div style={bannerStyle(imageUrl)}>
-      <h1 style={{ fontSize: "4rem" }}>{title}</h1>
+    <div
+      style={{ backgroundImage: `url(${imageUrl})` }}
+      className="h-[500px] bg-cover bg-center flex items-end justify-start p-8"
+    >
+      <h1 className="px-15 py-5 text-white text-7xl font-bold drop-shadow-[2px_2px_4px_rgba(0,0,0,0.7)]">
+        {title}
+      </h1>
     </div>
   );
 };
---
+++ client/src/components/ColorFilter.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const ColorFilter = ({ colors, selectedColors, onColorChange }) => {
+  return (
+    <div className="mb-8 border border-gray-300 rounded-lg py-7 px-5 shadow-xl/20">
+      <h3 className="text-3xl font-serif font-bold text-gray-800 mb-2">
+        Color
+      </h3>
+
+      <div className="relative h-1 mb-6">
+        <div className="absolute w-full h-0.5 bg-gray-300 bottom-0"></div>
+        <div className="absolute w-20 h-1 bg-yellow-400 bottom-0"></div>
+      </div>
+
+      <div className="space-y-3">
+        {colors.map((color) => (
+          <label
+            key={color.name}
+            className="flex items-center text-lg text-gray-700 cursor-pointer"
+          >
+            <input
+              type="checkbox"
+              name={color.name}
+              checked={selectedColors.includes(color.name)}
+              onChange={() => onColorChange(color.name)}
+              className="
+                form-checkbox 
+                h-5 w-5 
+                rounded 
+                border-gray-400 
+                text-yellow-500 
+                focus:ring-yellow-400
+              "
+            />
+            <span className="ml-3">
+              {color.name} ({color.count})
+            </span>
+          </label>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ColorFilter;
