598ecf37df271a9cc677d364b2372ca158652e7b
Ngô Bảo Long <nblong23@.clc.fitus.edu.vn>
2025-11-11 02:16:25 +0700

    update: carousel

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -1,5 +1,19 @@
+import ProductCarousel from "../components/ProductCarousel.jsx";
+
 const ProductDetails = () => {
-  return <div>HEELOOOO</div>;
+  const images = [
+    "/image/detail_1.jpg",
+    "/image/detail_2.jpg",
+    "/image/detail_3.jpg",
+    // "/image/detail_back.jpg",
+    // "/image/detail_front.jpg",
+  ];
+
+  return (
+    <div className="p-10">
+      <ProductCarousel images={images} />
+    </div>
+  );
 };
 
 export default ProductDetails;
