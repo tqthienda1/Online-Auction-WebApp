f50b575efc4c9e041fa5ddd64871f561dceb99db
Tran Quoc Thien <aloxezthien2005@gmail.com>
2025-11-13 23:21:17 +0700

    Update: Animation changing slide in ImageCarousel

--- client/src/components/ImageCarousel.jsx
+++ client/src/components/ImageCarousel.jsx
@@ -1,10 +1,46 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, EffectCoverflow } from "swiper/modules";
+import { useState, useRef } from "react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-const ImageCarousel = ({ images }) => {
+const ImageCarousel = ({ products }) => {
+  //scale bar width with slide width
+  const [slideWidth, setSlideWidth] = useState("400px");
+
+  //for changing slide animation
+  const [isUp, setIsUp] = useState(false);
+  const [isHide, setIsHide] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
+  const swiperRef = useRef(null);
+
+  const handleSlideChange = async (index) => {
+    //Prevent user click continuously
+    if (isAnimating) return;
+
+    //slide is changing
+    setIsAnimating(true);
+
+    //hide info bar
+    setIsUp(true);
+    await new Promise((resolve) => setTimeout(resolve, 600));
+    setIsHide(true);
+
+    //changing slide animation and wait
+    swiperRef.current.slideTo(index);
+    await new Promise((resolve) => setTimeout(resolve, 600));
+
+    //show info bar again
+    setIsHide(false);
+    setIsUp(false);
+
+    //after show is done, stop changing slide animation
+    setTimeout(() => {
+      setIsAnimating(false);
+    }, 600);
+  };
+
   return (
     <div className="flex flex-col my-10 justify-center items-center overflow-hidden">
       <h1 className="font-playfair text-5xl my-7 font-semibold">Ending Soon</h1>
@@ -22,28 +58,48 @@ const ImageCarousel = ({ images }) => {
           modifier: 1,
           slideShadows: true,
         }}
-        allowTouchMove={false} 
-        slideToClickedSlide
+        allowTouchMove={false}
+        slideToClickedSlide={false}
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
         onClick={(swiper) => {
           if (swiper.clickedIndex !== undefined) {
-            swiper.slideTo(swiper.clickedIndex);
+            handleSlideChange(swiper.clickedIndex);
           }
+        }}
+        onInit={(swiper) => {
+          const activeSlide = swiper.slides[swiper.activeIndex];
+
+          setSlideWidth(activeSlide.offsetWidth);
+        }}
+        onSlideChange={(swiper) => {
+          console.log("Active slide index:", swiper.activeIndex);
+          console.log(
+            "Centered slide element:",
+            swiper.slides[swiper.activeIndex]
+          );
         }}
         className="w-full h-[500px]"
       >
-        {images.map((item, index) => (
+        {products.map((item, index) => (
           <SwiperSlide
             key={index}
-            className="aspect-3/4 h-full w-[400px] rounded-2xl "
+            className="relative z-10 cursor-pointer pointer-events-auto aspect-3/4 h-full w-[400px] rounded-t-2xl "
           >
             <img
-              src={item}
+              src={item.image}
               alt=""
-              className="relative z-10 cursor-pointer pointer-events-auto w-full h-full object-cover rounded-2xl "
+              className=" w-full h-full object-cover rounded-t-2xl select-none"
+              draggable={false}
             />
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        className={`"h-full aspect-6/3 bg-brand rounded-b-2xl transform transition-transform duration-500 ${
+          isUp ? "-translate-y-full" : "translate-y-0"
+        } ${isHide ? "opacity-0" : "opacity-full"}`}
+        style={{ width: slideWidth }}
+      ></div>
     </div>
   );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,64 @@
 import ImageCarousel from "../components/ImageCarousel";
 
 const mockUpData = {
-  images: [
-    "/image/vangogh.jpg",
-    "/image/buddha.jpg",
-    "/image/complex.jpg",
-    "/image/food.jpg",
-    "/image/art.jpg",
+  products: [
+    {
+      image: "/image/vangogh.jpg",
+      name: "Vangogh",
+      currentPrice: 30000,
+      highestBidder: "thiendepzai",
+      buyNowPrice: 50000,
+      startDate: "13/07/2025",
+      endDate: "30/10/2025",
+      totalBids: 10,
+    },
+    {
+      image: "/image/buddha.jpg",
+      name: "Vangogh",
+      currentPrice: 30000,
+      highestBidder: "thiendepzai",
+      buyNowPrice: 50000,
+      startDate: "13/07/2025",
+      endDate: "30/10/2025",
+      totalBids: 10,
+    },
+    {
+      image: "/image/complex.jpg",
+      name: "Vangogh",
+      currentPrice: 30000,
+      highestBidder: "thiendepzai",
+      buyNowPrice: 50000,
+      startDate: "13/07/2025",
+      endDate: "30/10/2025",
+      totalBids: 10,
+    },
+    {
+      image: "/image/food.jpg",
+      name: "Vangogh",
+      currentPrice: 30000,
+      highestBidder: "thiendepzai",
+      buyNowPrice: 50000,
+      startDate: "13/07/2025",
+      endDate: "30/10/2025",
+      totalBids: 10,
+    },
+    {
+      image: "/image/art.jpg",
+      name: "Vangogh",
+      currentPrice: 30000,
+      highestBidder: "thiendepzai",
+      buyNowPrice: 50000,
+      startDate: "13/07/2025",
+      endDate: "30/10/2025",
+      totalBids: 10,
+    },
   ],
 };
 
 const Home = () => {
   return (
     <div className="">
-      <ImageCarousel images={mockUpData.images} />
+      <ImageCarousel products={mockUpData.products} />
     </div>
   );
 };
