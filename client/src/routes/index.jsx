b967d2567b1c0b38e689f0c308e81639e9cda2c2
Thinh <thinh367800@gmail.com>
2025-11-29 23:01:22 +0700

    Add: route for admin pages

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -2,12 +2,19 @@ import Home from "../pages/Home";
 import MainLayout from "../layouts/MainLayout";
 import BlankLayout from "../layouts/BlankLayout";
 import UserProfileLayout from "../layouts/UserProfileLayout";
+import AdminLayout from "@/layouts/AdminLayout";
+
 import CategoryPage from "../pages/CategoryPage";
 import ProductDetails from "../pages/ProductDetails";
 import LogInPage from "../pages/LogInPage";
 import SignUpPage from "../pages/SignUpPage";
 import UserProfile from "../pages/UserProfile";
 import AddProductsPage from "../pages/AddProductsPage";
+import AdminControlPanel from "@/pages/AdminControlPanel";
+import AdminCategoriesPage from "@/pages/AdminCategoriesPage";
+import AdminProductsPage from "@/pages/AdminProductsPage";
+import AdminUsersPage from "@/pages/AdminUsersPage";
+
 const guestRoutes = [
   { path: "/", component: Home, layout: MainLayout },
   { path: "/products", component: ProductDetails, layout: MainLayout },
@@ -15,6 +22,18 @@ const guestRoutes = [
   { path: "/login", component: LogInPage, layout: BlankLayout },
   { path: "/add-products", component: AddProductsPage, layout: MainLayout },
   { path: "/user_profile", component: UserProfile, layout: UserProfileLayout },
+  { path: "/admin", component: AdminControlPanel, layout: AdminLayout },
+  {
+    path: "/admin/categories",
+    component: AdminCategoriesPage,
+    layout: AdminLayout,
+  },
+  {
+    path: "/admin/products",
+    component: AdminProductsPage,
+    layout: AdminLayout,
+  },
+  { path: "/admin/users", component: AdminUsersPage, layout: AdminLayout },
   { path: "/:category", component: CategoryPage, layout: MainLayout },
 ];
 
