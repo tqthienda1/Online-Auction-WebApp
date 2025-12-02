0667e107b9c75b14617bbc91597ec31de5b47e51
Ngô Bảo Long <nblong23@clc.fitus.edu.vn>
2025-12-02 19:17:53 +0700

    fix: prisma7 -> prisma6

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signUp } from "../controllers/authController";
+import { signUp } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
+router.post("/signin", signin);
 
 export default router;
