ceb992065fc1ac727f132151b01a7e1cfc191bd8
Thinh <thinh367800@gmail.com>
2025-12-03 01:23:01 +0700

    Add: CRUD for Comment

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { signUp } from "../controllers/authController.js";
+import { signin } from "../controllers/authController.js";
 
 const router = express.Router();
 
--- server/routes/comment.routes.js
+++ server/routes/comment.routes.js
@@ -1,8 +1,16 @@
 import express from "express";
+import {
+  getAllComment,
+  createComment,
+  updateComment,
+  deleteComment,
+} from "../controllers/commentController.js";
 
-const router = express();
+const router = express.Router();
 
 router.post("/", getAllComment);
 router.post("/create", createComment);
 router.post("/update", updateComment);
 router.post("/delete", deleteComment);
+
+export default router;
