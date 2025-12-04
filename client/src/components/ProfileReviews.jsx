622b1b5682bf0cb3e16129d4834c0252c5ad2949
Thinh <thinh367800@gmail.com>
2025-12-04 23:27:42 +0700

    Update: ProfileTab

--- client/src/components/ProfileReviews.jsx
+++ client/src/components/ProfileReviews.jsx
@@ -5,9 +5,9 @@ import ProfileReviewCard from "./ProfileReviewCard";
 const ProfileReviews = () => {
   return (
     <div className="flex flex-col gap-5 text-brand w-3/4 rounded-2xl p-10 border border-gray-200 shadow-md mb-5">
-      <h1 className="font-bold text-2xl font-playfair">Reviews and Comments</h1>
+      <h1 className="font-bold text-2xl">Reviews and Comments</h1>
       <ProfileRating />
-      <h3 className="font-semibold text-lg font-playfair">Recent reviews</h3>
+      <h3 className="font-semibold text-lg">Recent reviews</h3>
       <ProfileReviewCard />
       <ProfileReviewCard />
     </div>
