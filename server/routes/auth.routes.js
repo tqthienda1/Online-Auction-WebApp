fff0aa7a996b24be319e6b28e1ab43180939f758
Ngô Bảo Long <nblong23@clc.fitus.edu.vn>
2025-12-06 01:20:15 +0700

    add: changePassword, verifyEmail. update: schema User

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { signUp, signIn } from "../controllers/authController.js";
+import {
+  signUp,
+  signIn,
+  verifyEmail,
+  changePassword,
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
+router.get("/verify", verifyEmail);
+router.patch("/change-password", changePassword);
 
 export default router;
