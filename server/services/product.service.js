31c2904d29a18652873261e3d89ee399101182e4
Tran Quoc Thien <aloxezthien2005@gmail.com>
2025-12-16 16:21:54 +0700

    Add: default current price

--- server/services/product.service.js
+++ server/services/product.service.js
@@ -205,6 +205,7 @@ export const addProduct = async (userId, body, files) => {
       category: {
         connect: { name: finalCategoryName },
       },
+      currentPrice: Number(startingPrice),
       startingPrice: Number(startingPrice),
       bidStep: Number(bidStep),
       buyNowPrice: buyNowPrice ? Number(buyNowPrice) : null,
