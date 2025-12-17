e565ef3418ef6fb9b0a75b1fb4c8f5060e150dfa
Thinh <thinh367800@gmail.com>
2025-12-17 15:44:54 +0700

    Update: CategoryController vs CategoryService

--- server/services/category.service.js
+++ server/services/category.service.js
@@ -52,18 +52,31 @@ export const CategoryService = {
   },
 
   async getTree() {
-    return await prisma.category.findMany({
+    const categories = await prisma.category.findMany({
       where: { parentID: null },
-      include: { categoryChild: true },
-    });
-  },
-
-  async getCategoryWithProducts(id) {
-    return await prisma.category.findUnique({
-      where: { id },
       include: {
-        product: true,
+        _count: {
+          select: { product: true },
+        },
+        categoryChild: {
+          include: {
+            _count: {
+              select: { product: true },
+            },
+          },
+        },
       },
     });
+
+    return categories.map((c) => ({
+      id: c.id,
+      name: c.name,
+      total: c._count.product,
+      categoryChild: c.categoryChild.map((child) => ({
+        id: child.id,
+        name: child.name,
+        total: child._count.product,
+      })),
+    }));
   },
 };
