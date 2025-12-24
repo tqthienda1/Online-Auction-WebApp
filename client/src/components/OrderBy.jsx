57c58cff6f3986104973e74293d5901b4d8e3fa0
Thinh <thinh367800@gmail.com>
2025-12-24 15:46:15 +0700

    Add: OrderBy

---
+++ client/src/components/OrderBy.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
+
+const OrderBy = ({ onSortChange }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState("Order by");
+
+  const options = [
+    { label: "Price Low to High", value: "asc", icon: <FaArrowUp /> },
+    { label: "Price High to Low", value: "desc", icon: <FaArrowDown /> },
+    // {label: "Most Popular", value:"totalBid", icon: }
+  ];
+
+  const handleSelect = (option) => {
+    setSelected(option.label);
+    setIsOpen(false);
+    onSortChange(option.value);
+  };
+
+  return (
+    <div className="w-full relative inline-block text-left mb-8 border border-gray-300 rounded-lg py-7 px-5 shadow-xl/20">
+      <h3 className="text-3xl font-playfair font-bold text-gray-800 mb-2">
+        Order By
+      </h3>
+
+      <div className="relative h-1 mb-6">
+        <div className="absolute w-full h-0.5 bg-gray-300 bottom-0"></div>
+        <div className="absolute w-16 h-1 bg-yellow-400 bottom-0"></div>
+      </div>
+
+      <button
+        type="button"
+        onClick={() => setIsOpen(!isOpen)}
+        className="w-full font-semibold bg-yellow-400 border border-gray-300 rounded-md shadow-sm px-4 py-2 text-white flex justify-between items-center hover:bg-yellow-500 focus:outline-none"
+      >
+        {selected}
+        <svg
+          className={`h-5 w-5 transition-transform ${
+            isOpen ? "rotate-180" : ""
+          }`}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10 14a1 1 0 01-.707-.293l-5-5a1 1 0 111.414-1.414L10 11.586l4.293-4.293a1 1 0 011.414 1.414l-5 5A1 1 0 0110 14z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+
+      {isOpen && (
+        <div className="absolute mt-1 w-[85%] bg-gray-100 shadow-lg rounded-md z-10">
+          {options.map((option) => (
+            <div
+              key={option.value}
+              onClick={() => handleSelect(option)}
+              className="cursor-pointer px-4 py-2 flex items-center gap-2 hover:bg-gray-200"
+            >
+              {option.icon}
+              <span>{option.label}</span>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default OrderBy;
