5e9d453bac0fe7645d1370dcf0527adb5c572d4c
Ngô Bảo Long <nblong23@clc.fitus.edu.vn>
2026-01-04 14:44:57 +0700

    update: mask Highest User

--- client/src/helper/maskUser.js
+++ client/src/helper/maskUser.js
@@ -1,9 +1,14 @@
 export function maskUsername(username) {
-  if (!username) return null;
-  if (username.length <= 3) return username[0] + "**";
+  if (!username) return "";
 
-  const first = username.slice(0, 2);
-  const last = username.slice(-2);
+  const len = username.length;
 
-  return `${first}***${last}`;
+  if (len <= 4) return username[0] + "*".repeat(len - 1);
+
+  const keep = Math.ceil(len * 0.3); // giữ 30%
+  const start = username.slice(0, keep);
+  const end = username.slice(-keep);
+  const masked = "*".repeat(len - keep * 2);
+
+  return start + masked + end;
 }
